5a809fcbde285c47894e141aa86e4ac6b5d9d020
Anurag Dwivedi <anuragd7@gmail.com>
2019-08-27 10:00:17 +0530

    print styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'print';
+
+  print(): void {
+    let printContents, popupWin;
+    printContents = document.getElementById('print-section').innerHTML;
+    // const sib1 = document.getElementById('print-section').previousSibling;
+    // const sib1 = document.getElementById('print-section').previousSibling;
+    popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
+    popupWin.document.open();
+    popupWin.document.write(`
+      <html>
+        <head>
+          <title>Print tab</title>
+          <style>
+          @page {
+            size: auto;
+            margin:45;
+          }
+          .header, .hide { visibility: hidden }
+          </style>
+        </head>
+    <body onload="window.print();window.close()">${printContents}</body>
+      </html>`
+    );
+    popupWin.document.close();
+}
 }
